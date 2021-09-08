952b9b1128bccfad0adf4a8a4f7fca35c5a92311
LachowskiTomekPL <lachowskitomasz.pl@gmail.com>
2021-09-08 11:26:54 +0200

    Creating first filter

---
+++ src/main/java/com/example/springproject/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.example.springproject.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @RequestMapping("/hello")
+    public String index(){
+        return "Hello World";
+    }
+}
