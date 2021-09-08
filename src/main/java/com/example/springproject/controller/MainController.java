212145e430c16ac641e890a4e952842174a8eea0
LachowskiTomekPL <lachowskitomasz.pl@gmail.com>
2021-09-08 16:07:43 +0200

    adding an Optional in the method quantityOverAnyValue as a parameter to avoid error in case wrong value

--- src/main/java/com/example/springproject/controller/MainController.java
+++ src/main/java/com/example/springproject/controller/MainController.java
@@ -1,30 +1,35 @@
 package com.example.springproject.controller;
 
 import com.example.springproject.entity.Item;
-import com.example.springproject.repository.ItemRepository;
 import com.example.springproject.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class MainController {
-@Autowired
+    @Autowired
     ItemService itemService;
 
 
     @RequestMapping("/quantity")
-    public List<Item> index(HttpServletResponse response){
+    public List<Item> quantityOverTwenty(HttpServletResponse response) {
 
         return itemService.getItemswithQuantityOverTwenty();
     }
 
     @RequestMapping("/quantityOver")
-    public List<Item> index(){
+    public List<Item> quantityOverAnyValue(@RequestParam(name = "quantity") Optional<Integer> quantityParam) {
+        int quantity = 100;
+        if (quantityParam.isPresent()) {
+            quantity = quantityParam.get();
+        }
 
-        return itemService.getItemWithQuantityOver(50);
+        return itemService.getItemWithQuantityOver(quantity);
     }
 }
